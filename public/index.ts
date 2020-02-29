9bac90d372774ad674be0dc838f96af69153b7e2
Irfan <53381250+mdirfan-haja@users.noreply.github.com>
2020-02-29 21:34:22 +0000

    update on version

--- public/index.ts
+++ public/index.ts
@@ -1,1 +1,31 @@
-console.log('sdsadsadsad')
+import firebase from 'firebase/app'
+import 'firebase/storage'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDR-0c96A_yzfmX8jpUv5Bt5cfXe7LXDGw",
+    authDomain: "music-playlist-d8e60.firebaseapp.com",
+    databaseURL: "https://music-playlist-d8e60.firebaseio.com",
+    projectId: "music-playlist-d8e60",
+    storageBucket: "music-playlist-d8e60.appspot.com",
+    messagingSenderId: "774310292709",
+    appId: "1:774310292709:web:5e270cb8f5f00dcb33380c",
+    measurementId: "G-7WBCW3YCES"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var uploader = document.getElementById('uploadbar')
+var filesbutton = document.getElementById('filesbutton')
+
+ function uploadfile() {  filesbutton.addEventListener('change',function (e){
+
+var file = e.target.files[0]
+
+var storageRef = firebase.storage().ref('music_list/' + file.name)
+
+storageRef.put(file)
+
+ })
+return(uploadfile)
+}
